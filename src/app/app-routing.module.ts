c3d8e0ed43412ad1490fefa8d18f6c0a23ab9f1f
ruchitha211 <polaruchitha796@gmail.com>
2020-04-02 11:52:40 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeModule } from './home/home.module';
+import { HeaderModule } from './header/header.module';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'header',component:HeaderComponent},
+  {path:'**',component:HomeComponent}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+  HomeModule,
+HeaderModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
